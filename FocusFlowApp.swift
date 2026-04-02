3ce141911e094e276436ebb0eee9afb2b9bd2e62
alambdcse <alambdcse@gmail.com>
2026-04-02 11:10:23 -0700

    FocusFlowApp.swift

--- FocusFlowApp.swift
+++ FocusFlowApp.swift
@@ -1,13 +1,15 @@
-import SwiftUI
+mport SwiftUI
 
 @main
 struct FocusFlowApp: App {
     @StateObject private var habitStore = HabitStore()
+    @StateObject private var subscriptionManager = SubscriptionManager()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(habitStore)
+                .environmentObject(subscriptionManager)
         }
     }
 }
